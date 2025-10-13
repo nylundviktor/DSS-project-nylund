1360c509404a0070d19f9e6b93f7119f48889aaf
Viktor Nylund <nylunvik@arcada.fi>
2025-10-13 15:59:55 +0300

    adapt code base to project, add screenshots, write notes

--- recommender.py
+++ recommender.py
@@ -7,84 +7,139 @@ from sklearn.decomposition import NMF
 from sklearn.model_selection import train_test_split
 
 class Recommender:
-    def __init__(self, movie_data_path, rating_data_path):
-        self.movies_df = pd.read_csv(movie_data_path)
-        self.ratings_df = pd.read_csv(rating_data_path)
-        self.content_recommender = self._ContentBasedRecommender(self.movies_df)
-        
-        self.nmf_model = None; self.user_item_matrix = None; self.user_mapper = None
-        self.movie_mapper = None; self.movie_inv_mapper = None
+    def __init__(self, games_data_path, recommendations_data_path):
+        self.games_df = pd.read_csv(games_data_path)
+        self.recommendations_df = pd.read_csv(recommendations_data_path)
+        self.content_recommender = self._ContentBasedRecommender(self.games_df)
+        self.recommendations_df['is_recommended'] = self.recommendations_df['is_recommended'].astype(int)
+        # 'features' column from game attributes (rating, positive ratio, platforms)
+        # this text is used by the content-based recommender's TF-IDF vectorizer to find similarity
+        self.games_df['features'] = (
+            self.games_df['rating'].fillna('') + ' ' +
+            self.games_df['positive_ratio'].astype(str) + ' ' +
+            self.games_df['win'].apply(lambda x: 'win' if x else '') + ' ' +
+            self.games_df['mac'].apply(lambda x: 'mac' if x else '') + ' ' +
+            self.games_df['linux'].apply(lambda x: 'linux' if x else '')
+        )
 
-    def fit(self, train_df):
-            self.ratings_df = train_df
+        self.nmf_model = None; self.user_item_matrix = None
+        self.user_mapper = None; self.game_mapper = None
+        self.game_inv_mapper = None; self.recs_train_df = None
+
+    def fit(self, recs_train_df):
+            self.recs_train_df = recs_train_df # recommendations, user-game interaction
             self.content_recommender.fit()
-            self.user_item_matrix = train_df.pivot_table(index='userId', columns='movieId', values='rating').fillna(0)
+            self.user_item_matrix = recs_train_df.pivot_table(index='user_id', columns='app_id', values='is_recommended').fillna(0)
             self.user_mapper = {uid: i for i, uid in enumerate(self.user_item_matrix.index)}
-            self.movie_mapper = {mid: i for i, mid in enumerate(self.user_item_matrix.columns)}
+            self.game_mapper = {mid: i for i, mid in enumerate(self.user_item_matrix.columns)}
             self.nmf_model = NMF(n_components=20, init='random', random_state=42, max_iter=500)
             self.nmf_model.fit(self.user_item_matrix)
 
-    def recommend(self, user_id, movie_title_seed, num_recommendations=10):
-        content_recs = self.content_recommender.recommend(movie_title_seed, num_recommendations)
+    def recommend(self, user_id, game_title_seed, num_recommendations=10):
+        content_recs = self.content_recommender.recommend(game_title_seed, num_recommendations)
         collaborative_recs = []
         if user_id in self.user_mapper:
             user_idx = self.user_mapper[user_id]
-            user_vector = self.user_item_matrix.iloc[user_idx].values.reshape(1, -1)
+            user_vector = self.user_item_matrix.loc[user_idx].values.reshape(1, -1)
             user_P = self.nmf_model.transform(user_vector)
             item_Q = self.nmf_model.components_
             predicted_scores = np.dot(user_P, item_Q).flatten()
             scores_series = pd.Series(predicted_scores, index=self.user_item_matrix.columns)
-            rated_movies = self.ratings_df[self.ratings_df['userId'] == user_id]['movieId']
-            scores_series = scores_series.drop(index=rated_movies, errors='ignore')
-            top_movie_ids = scores_series.nlargest(num_recommendations).index.tolist()
-            collaborative_recs = self.movies_df[self.movies_df['movieId'].isin(top_movie_ids)]['title'].tolist()
+            rated_games = self.recommendations_df[self.recommendations_df['user_id'] == user_id]['app_id']
+            scores_series = scores_series.drop(index=rated_games, errors='ignore')
+            top_game_ids = scores_series.nlargest(num_recommendations).index.tolist()
+            collaborative_recs = self.games_df[self.games_df['app_id'].isin(top_game_ids)]['title'].tolist()
         combined_recs = collaborative_recs + content_recs
         unique_recs = list(dict.fromkeys(combined_recs).keys())
         return unique_recs[:num_recommendations]
 
 
     class _ContentBasedRecommender:
-        def __init__(self, movies_df): 
-            self.movies_df = movies_df
+        def __init__(self, games_df): 
+            self.games_df = games_df
             self.tfidf_matrix = None
-            self.movie_indices = None
+            self.game_indices = None
         
         def fit(self):
-            self.movies_df['genres'] = self.movies_df['genres'].fillna('')
+            self.games_df['features'] = self.games_df['features'].fillna('')
             tfidf_vectorizer = TfidfVectorizer(stop_words='english')
-            self.tfidf_matrix = tfidf_vectorizer.fit_transform(self.movies_df['genres'])
-            self.movie_indices = pd.Series(self.movies_df.index, index=self.movies_df['title']).drop_duplicates()
+            self.tfidf_matrix = tfidf_vectorizer.fit_transform(self.games_df['features'])
+            self.game_indices = pd.Series(self.games_df.index, index=self.games_df['title']).drop_duplicates()
         
-        def recommend(self, movie_title, num_recommendations=10):
-            if movie_title not in self.movie_indices: return []
-            movie_index = self.movie_indices[movie_title]
-            movie_vector = self.tfidf_matrix[movie_index]
-            similarity_scores = cosine_similarity(movie_vector, self.tfidf_matrix).flatten()
-            movie_similarity_list = list(enumerate(similarity_scores))
-            sorted_similar_movies = sorted(movie_similarity_list, key=lambda x: x[1], reverse=True)
-            top_movie_indices = [i[0] for i in sorted_similar_movies[1:num_recommendations + 1]]
-            return self.movies_df['title'].iloc[top_movie_indices].tolist()
+        def recommend(self, game_title, num_recommendations=10):
+            if game_title not in self.game_indices: 
+                return []
+            game_index = self.game_indices[game_title]
+            game_vector = self.tfidf_matrix[game_index]
+            similarity_scores = cosine_similarity(game_vector, self.tfidf_matrix).flatten()
+            
+            game_similarity_list = list(enumerate(similarity_scores))
+            sorted_similar_games = sorted(game_similarity_list, key=lambda x: x[1], reverse=True)
+            
+            top_game_indices = [i[0] for i in sorted_similar_games[1:num_recommendations + 1]]
+            return self.games_df['title'].iloc[top_game_indices].tolist()
 
 
 if __name__ == '__main__':
-    print("---Collaborative filtering recommendations---\n")
-    # Simple main function to test
     # Create an instance of the recommender
-    recommender = Recommender(movie_data_path='./data/movies.csv', rating_data_path='./data/ratings.csv')
-    
-    # Fit the model
-    recommender.fit()
-    
-    # Get and print recommendations for a movie and user
-    #movie_title_to_test = "Nausicaä of the Valley of the Wind (Kaze no tani no Naushika) (1984)"
-    movie_title_to_test = "Toy Story (1995)"
-    user_id_to_test = 1
+    recommender = Recommender(
+        games_data_path='./data/games.csv', 
+        recommendations_data_path='./data/recommendations.csv'
+    )
 
-    recommendations = recommender.recommend(user_id_to_test, movie_title_to_test)
+    # Count number of ratings per user
+    user_counts = recommender.recommendations_df['user_id'].value_counts()
+
+    # Filter users with at least 3 ratings
+    active_users = user_counts[user_counts >= 3].index
+
+    # Filter recommendations to only include those users
+    filtered_recommendations = recommender.recommendations_df[
+        recommender.recommendations_df['user_id'].isin(active_users)
+    ]
+    recommender.recommendations_df = filtered_recommendations
+
+    # REMINDER fit with training data = recommendations
+    train_data = []
+    test_data = []
+
+    '''
+    # group() is faster
+    for user_id, user_ratings in recommender.recommendations_df.groupby('user_id'):
+        if len(user_ratings) < 2:
+            # Cannot split, only one row or ratings
+            test_data.append(user_ratings)
+            continue
+        train, test = train_test_split(user_ratings, test_size=0.2, random_state=42)
+        train_data.append(train)
+        test_data.append(test)
+
+    '''
+    print(recommender.recommendations_df['user_id'].nunique())
+
+    for user_id in recommender.recommendations_df['user_id'].unique()[:50]:
+        user_ratings = recommender.recommendations_df[ recommender.recommendations_df['user_id'] == user_id ]
+        if len(user_ratings) < 2:
+            # Cannot split, only one row or ratings
+            test_data.append(user_ratings)
+            continue
+        train, test = train_test_split(user_ratings, test_size=0.2, random_state=42)
+        train_data.append(train)
+        test_data.append(test)
+
+    train_df = pd.concat(train_data)
+    test_df = pd.concat(test_data)
+
+    recommender.fit(train_df)
+    
+    test_user_id=2586
+    test_game_title="LIMBO"
+
+    recommendations = recommender.recommend(test_user_id, test_game_title)
     
     if recommendations:
-        print(f"\nRecommendations for user {user_id_to_test} based on '{movie_title_to_test}':")
-        for movie in recommendations:
-            print(f"- {movie}")
+        print(f"\nRecommendations for user {test_user_id} based on '{test_game_title}':")
+        for _recommendation in recommendations:
+            print(f"- {_recommendation}")
     else:
         print("No recommendations found.")
---
+++ report.md
@@ -0,0 +1,22 @@
+# Kursprojekt - Rapport
+
+*Viktor Nylund - Beslutsstödsystem*
+
+
+---
+
+## EDA & Rekommendationssystem
+
+På grund av storleken på users filen (13781059 users) tog programet rätt så länge att köra.   Detta var resultaten när jag begränsade det till att endast använda 50 users data. 
+
+<img src="images/out1.png.png" alt="Screenshot of output" width="700"/>
+
+<img src="images/code1.png.png" alt="Screenshot of output" width="700"/>
+
+Jag bestämde att begränsa det till att endast använda users med minst 3 ratings för att se om det är mer hanterligt. Därtill berättade AI att `group()` är snabbare. 
+
+
+
+---
+
+
