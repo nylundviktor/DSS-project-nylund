1d9822b2b1fa6ac63bc7cf4be5388c892a47659a
Viktor Nylund <nylunvik@arcada.fi>
2025-10-14 17:24:53 +0300

    create seperate function for collaborative filtering

--- recommender.py
+++ recommender.py
@@ -28,11 +28,13 @@ def price_to_category(price):
 
 
 class Recommender:
-    # Collaborative filtering (user interaction data, finds games from patterns between users)
     def __init__(self, games_data_path, recommendations_data_path):
         self.games_df = pd.read_csv(games_data_path)
         self.recommendations_df = pd.read_csv(recommendations_data_path)
+
         self.content_recommender = self._ContentBasedRecommender(self.games_df)
+        self.collaborative_recommender = self._CollaborativeRecommender(self.games_df, self.recommendations_df)
+    
         self.recommendations_df['is_recommended'] = self.recommendations_df['is_recommended'].astype(int)
         # 'features' column from game attributes (rating, positive ratio, platforms)
         # this text is used by the content-based recommender's TF-IDF vectorizer to find similarity
@@ -41,42 +43,72 @@ class Recommender:
             self.games_df['positive_ratio'].astype(str) + ' ' +
             self.games_df['price_final'].apply(price_to_category) + ' ' +
             self.games_df['user_reviews'].apply(reviews_to_category) + ' ' +
-            self.games_df['discount'].apply(lambda d: 'discounted' if d > 0 else 'full_price') + ' ' +
             self.games_df['win'].apply(lambda x: 'win' if x else '') + ' ' +
             self.games_df['mac'].apply(lambda x: 'mac' if x else '') + ' ' +
             self.games_df['linux'].apply(lambda x: 'linux' if x else '')
         )
 
-        self.nmf_model = None; self.user_item_matrix = None
-        self.user_mapper = None; self.game_mapper = None
-        self.game_inv_mapper = None; self.recs_train_df = None
-
     def fit(self, recs_train_df):
-            self.recs_train_df = recs_train_df # recommendations, user-game interaction
+            #self.recs_train_df = recs_train_df # recommendations, user-game interaction
             self.content_recommender.fit()
-            self.user_item_matrix = recs_train_df.pivot_table(index='user_id', columns='app_id', values='is_recommended').fillna(0)
-            self.user_mapper = {uid: i for i, uid in enumerate(self.user_item_matrix.index)}
-            self.game_mapper = {mid: i for i, mid in enumerate(self.user_item_matrix.columns)}
-            self.nmf_model = NMF(n_components=20, init='random', random_state=42, max_iter=500)
-            self.nmf_model.fit(self.user_item_matrix)
+            self.collaborative_recommender.fit(recs_train_df)
 
     def recommend(self, user_id, game_title_seed, num_recommendations=10):
         content_recs = self.content_recommender.recommend(game_title_seed, num_recommendations)
-        collaborative_recs = []
-        if user_id in self.user_mapper:
-            user_idx = self.user_mapper[user_id]
-            user_vector = self.user_item_matrix.loc[user_idx].values.reshape(1, -1)
-            user_P = self.nmf_model.transform(user_vector)
-            item_Q = self.nmf_model.components_
-            predicted_scores = np.dot(user_P, item_Q).flatten()
-            scores_series = pd.Series(predicted_scores, index=self.user_item_matrix.columns)
-            rated_games = self.recommendations_df[self.recommendations_df['user_id'] == user_id]['app_id']
-            scores_series = scores_series.drop(index=rated_games, errors='ignore')
-            top_game_ids = scores_series.nlargest(num_recommendations).index.tolist()
-            collaborative_recs = self.games_df[self.games_df['app_id'].isin(top_game_ids)]['title'].tolist()
+        collaborative_recs = self.collaborative_recommender.recommend(user_id, num_recommendations) 
+
         combined_recs = collaborative_recs + content_recs
-        unique_recs = list(dict.fromkeys(combined_recs).keys())
+        #unique_recs = list(dict.fromkeys(combined_recs).keys())
+        unique_recs = [r for r in dict.fromkeys(combined_recs).keys() if r != game_title_seed]
         return unique_recs[:num_recommendations]
+    
+    
+    # Collaborative filtering (user interaction data, finds games from patterns between users)
+    class _CollaborativeRecommender:
+        def __init__(self, games_df, recommendations_df):
+            self.games_df = games_df
+            self.recommendations_df = recommendations_df
+
+            self.nmf_model = None; self.user_item_matrix = None
+            self.user_mapper = None; self.game_mapper = None
+            self.game_inv_mapper = None
+
+        def fit(self, recs_train_df):
+            self.recs_train_df = recs_train_df # recommendations, user-game interaction
+            #self.user_item_matrix = recs_train_df.pivot_table(index='user_id', columns='app_id', values='is_recommended').fillna(0)
+            pivot = recs_train_df.pivot_table(
+                index='user_id', columns='app_id', values='is_recommended', fill_value=0
+            )
+            pivot = pivot.astype(pd.SparseDtype("float", fill_value=0)) # Convert to a sparse DataFrame
+            pivot_sparse = pivot.sparse.to_coo()
+            self.user_item_matrix = pivot
+
+
+            self.user_mapper = {uid: i for i, uid in enumerate(pivot.index)}
+            self.game_mapper = {mid: i for i, mid in enumerate(pivot.columns)}
+
+            self.nmf_model = NMF(n_components=20, init='random', solver='mu', random_state=42, max_iter=200)
+            self.nmf_model.fit(pivot_sparse)
+            #self.user_mapper = {uid: i for i, uid in enumerate(self.user_item_matrix.index)}
+            #self.game_mapper = {mid: i for i, mid in enumerate(self.user_item_matrix.columns)}
+            #self.nmf_model = NMF(n_components=20, init='random', random_state=42, max_iter=500)
+            #self.nmf_model.fit(self.user_item_matrix)
+
+        def recommend(self, user_id, num_recommendations=10):
+            collaborative_recs = []
+            if user_id in self.user_mapper:
+                user_idx = self.user_mapper[user_id]
+                user_vector = self.user_item_matrix.loc[user_idx].values.reshape(1, -1)
+                user_P = self.nmf_model.transform(user_vector)
+                item_Q = self.nmf_model.components_
+                predicted_scores = np.dot(user_P, item_Q).flatten()
+                scores_series = pd.Series(predicted_scores, index=self.user_item_matrix.columns)
+                rated_games = self.recommendations_df[self.recommendations_df['user_id'] == user_id]['app_id']
+                scores_series = scores_series.drop(index=rated_games, errors='ignore')
+                top_game_ids = scores_series.nlargest(num_recommendations).index.tolist()
+                collaborative_recs = self.games_df[self.games_df['app_id'].isin(top_game_ids)]['title'].tolist()
+            return collaborative_recs
+
 
     # Content based filtering (attributes and features, finds similar games)
     class _ContentBasedRecommender:
@@ -103,6 +135,12 @@ class Recommender:
             
             top_game_indices = [i[0] for i in sorted_similar_games[1:num_recommendations + 1]]
             return self.games_df['title'].iloc[top_game_indices].tolist()
+        
+    def get_content_based_recommendations(self, game_title, num_recommendations=10):
+        # Fit the content-based recommender (in case it's not fitted yet)
+        self.content_recommender.fit()
+        # Get recommendations
+        return self.content_recommender.recommend(game_title, num_recommendations)
 
 
 if __name__ == '__main__':
@@ -112,21 +150,27 @@ if __name__ == '__main__':
         recommendations_data_path='./data/recommendations.csv'
     )
 
-    # Count number of ratings per user
     user_counts = recommender.recommendations_df['user_id'].value_counts()
-
-    # Filter users with at least 3 ratings
-    active_users = user_counts[user_counts >= 5].index
-
-    # Filter recommendations to only include those users
+    print(f"User left: {len(user_counts)}")
+    # sample 1, filtered
+    active_users = user_counts[user_counts >= 10].index
+    filtered_sample = active_users.to_series().sample(n=60000, random_state=42)
     filtered_recommendations = recommender.recommendations_df[
-        recommender.recommendations_df['user_id'].isin(active_users)
+        recommender.recommendations_df['user_id'].isin(filtered_sample)
     ]
-    recommender.recommendations_df = filtered_recommendations
-
-    print(f"Rows left: {len(filtered_recommendations)}")
-
-    # REMINDER fit with training data = recommendations
+    print(f"After filtering: {len(filtered_recommendations)}")
+    
+    # sample 2, random
+    all_users = user_counts.index
+    random_users = all_users.to_series().sample(n=10000, random_state=42)
+    random_sample = recommender.recommendations_df[
+        recommender.recommendations_df['user_id'].isin(random_users)
+    ]
+    # Combine samples
+    combined_sample = pd.concat([filtered_recommendations, random_sample]).drop_duplicates()
+    recommender.recommendations_df = combined_sample
+    print(f"Combined sample size (rows): {len(combined_sample)}")
+    print(f"Unique users in combined sample: {combined_sample['user_id'].nunique()}")
     train_data = []
     test_data = []
 
@@ -140,33 +184,27 @@ if __name__ == '__main__':
         train_data.append(train)
         test_data.append(test)
 
-    print(recommender.recommendations_df['user_id'].nunique())
-
-    '''
-    for user_id in recommender.recommendations_df['user_id'].unique()[:50]:
-        user_ratings = recommender.recommendations_df[ recommender.recommendations_df['user_id'] == user_id ]
-        if len(user_ratings) < 2:
-            # Cannot split, only one row or ratings
-            test_data.append(user_ratings)
-            continue
-        train, test = train_test_split(user_ratings, test_size=0.2, random_state=42)
-        train_data.append(train)
-        test_data.append(test)
-    '''
-
     train_df = pd.concat(train_data)
     test_df = pd.concat(test_data)
 
     recommender.fit(train_df)
-    
+
+
     test_user_id=2586
     test_game_title="LIMBO"
 
-    recommendations = recommender.recommend(test_user_id, test_game_title)
+    content_recs = recommender.get_content_based_recommendations(test_game_title, 10)
+    print(f"Content-based recommendations for '{test_game_title}':")
+    for rec in content_recs:
+        print("-", rec)
     
+    '''
+    recommendations = recommender.recommend(test_user_id, test_game_title)
+
     if recommendations:
         print(f"\nRecommendations for user {test_user_id} based on '{test_game_title}':")
         for _recommendation in recommendations:
             print(f"- {_recommendation}")
     else:
         print("No recommendations found.")
+    '''
--- report.md
+++ report.md
@@ -9,24 +9,62 @@
 
 ---
 
-## EDA & Rekommendationssystem
 
-På grund av storleken på users filen (13781059 users) tog programet rätt så länge att köra.   Detta var resultaten när jag begränsade det till att endast använda 50 users data. 
+## **Innehållsbaserad filtreringen**  
+Använder sig av spelens olika attribut, så som platform och pris, för att hitta likheter mellan spelen. Tyvärr finns inte genre i mitt data. 
+
+## **Kollaborativ filtrering**  
+Använder sig av användarnas rekommendationer och de spel de spelat för att hitta likheter mellan användarna och spel de antagligen skulle tycka om.
+
+Jag kombinerar dessa för att skapa ett hybridsystem som använder sig av båda faktorerna. Den innehållsbaserade listan sätts till med de kollaborativa rekommendationerna.  
+
+## Hybrid
+
+På grund av storleken på users filen (13781059 users) tog programet rätt så länge att köra.  
+Detta var resultaten när jag begränsade det till att endast använda 5 users data. 
 
 <img src="images/out1.png" alt="Screenshot of first output" width="700"/>
 
 <img src="images/code1.png" alt="Screenshot of filtering code" width="700"/>
 
-Jag bestämde att begränsa det till att endast använda users med minst 5 ratings för att se om det är mer hanterligt. Därtill berättade AI att `group()` är snabbare. 
+Jag bestämde att begränsa det till att använda users med minst 5 ratings för att se om det är mer hanterligt. 
+Därtill berättade AI att programmet blir snabbare med `group()` och att använda en gläsare 'sparse' user-item matris med hjälp av `scipy.sparse`. Denna använder mindre minne och gör träningen snabbare. Jag lade även till `user_reviews, price_final och discount` till features i hopp om att de ger bättre resultat. 
 
+<img src="images/imp1.png" alt="Screenshot of filtering code" width="700"/>
+
+Jag bestämde också att helt enkelt minska datamängden direkt till ett par 10 tusen. 
+Men jag ville också hålla datan så autentisk som möjligt, och vill inte endast använda mig av de spel med flest rekommendationer. Även om det försämrar inlärning, ville jag att systemet inte skulle förlita sig helt på användare som rekommenderat många spel.  
+Därför är 1/4 av den data jag använder mig av samplad på måfå, och den återstående andelen är användare med minst 5 rekommendationer.
+
+<img src="images/code2.png" alt="Screenshot of data splitting code" width="700"/>
+
+| Metric                  | Value      |
+|-------------------------|------------|
+| Användare               | 13,781,059 |
+| >=5 rekommendationer    | 879,211    |
+| Kombinerad data         | 952,526    |
+| Unika användare         | 99,844     |
+
+Det behövs evaluering för att veta säkert om detta är bättre eller inte. Det borde förbättra systemet ifall om användaren har väldigt få spel i sin lista. 
 
 ### Resultat
 
-**Innehållsbaserad filtreringen**  
-Använder sig av spelens olika attribut, så som platform och pris, för att hitta likheter mellan spelen. Tyvärr finns inte genre i mitt data. 
+Första
 
-**Kollaborativ filtrering**  
-Använder sig av användarnas rekommendationer och de spel de spelat för att hitta likheter mellan användarna och spel de antagligen skulle tycka om.
+<img src="images/out1.png" alt="Screenshot of first output" width="700"/>
+
+Andra
+
+<img src="images/out2.png" alt="Screenshot of second output" width="700"/>
+
+Förändringarna ovan gjorde programet mycket snabbare att köra. Det krävdes bara 2-3 minuter.  
+Vid första anblick blir jag dock något oroad att rekommendationssystemet försämrats, men många av spelen i listan är obekanta för mig. 
+T.ex. finns inte `The Binding of Isaac` kvar i listan, som var en passande rekommendation. `INSIDE`, uppföljaren, lyser även med sin frånvaro.  
+AI menade att `discount` kan vara en vilseledande feature, så jag exkluderade den. Jag ändrade också andelen data som samplades på måfå från 1/4 till 1/6. Detta så att andelen slumpad data inte har för stor inverkan. Jag ökade även gränsen för ratings till minst 10 per användare, vilket krävde att jag minskade mängden använd data en aning.  
+Resultatet var nära på det samma.
+
+<img src="images/out3.png" alt="Screenshot of third output" width="700"/>
+
 
 ---
 
